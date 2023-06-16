48716097468d0e8b8d9718c26d6238627fc32406
Sherifzade-Bajrami <sb51075@ubt-uni.net>
2023-06-17 01:02:10 +0200

    button

---
+++ client/src/components/Button.js
@@ -0,0 +1,21 @@
+const Button = ({
+    className = "",
+    type = "button",
+    label = "",
+    disabled = false,
+    onClick,
+  }) => {
+    return (
+      <button
+        type={type}
+        disabled={disabled}
+        onClick={() => onClick}
+        className={`${className} w-full py-2 text-center font-regular text-16 rounded-full`}
+      >
+        {label}
+      </button>
+    );
+  };
+  
+  export default Button;
+  
